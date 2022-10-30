75d814f901f0f717b3832029af0454e37bf72e1b
Mohammed Hamioui <r0880734@student.thomasmore.be>
2022-10-30 21:40:13 +0100

    first commit

---
+++ static/JS/carForm.js
@@ -0,0 +1,59 @@
+var cars= new Array();
+
+cars["Alfa Romeo"] = new Array("145","146","147","155","156","159","164","166","33","75","Alfetta","GT","GTV","GTV-6","GTV6","Guiletta","Milano","Spider");
+cars["Aston Martin"] = new Array("AMV8 Vantage","Bulldog","DB AR1","DB2","DB3 Spyder","DB3S","DB4","DB5","DB6","DB7","DB9","DB9 Sports Pack","DBR1","DBR4","DBR9","DBRS9","DBS","DP212","Lagonda","Le Mans","MK II Ulster Le","Prodrive","Rapide","Type C Speed Mod","Ulster","V12 Vanquish","V8","V8 Coupe","V8 Vantage","V8 Volante","Vanquish Roadste","Vanquish S","Vantage","Virage","Zagato DB4 GT");
+cars["Audi"] = new Array("100","100 C4 Series","100 V6","200","4000","5000","80","80 Cabriolet","90","A3","A4","A4 Cabriolet","A5","A6","A7","A8","Allroad","Allroad Quattro","Cabriolet","Coupe","Fox","Q5","Q7","Quattro","R8","RS 4","RS 6","S4","S6","S8","TT","V8");
+cars["Bentley"] = new Array("Amage","Arnage","Azure","Brooklands","Continental","Continental Flyi","Continental GT","Continental GTC","Continental R","Eight","GT","Mulsanne","Turbo","Turbo R");
+cars["BMW"] = new Array("1 Series","128","135","2000","2002","2800","3 Series","318","320","323","325","328","330","335","5 Series","525","528","530","533","535","540","545","550","6 Series","630","633","635","645","650","7 Series","733","735","740","745","750","760","840","850","Alpina","Bavaria","Isetta","L6","L7","M","M3","M5","M6","Motorcycle","X3","X5","X6","Z1","Z3","Z4","Z8");
+cars["Ferrari"] = new Array("125","159","166","195","196","208","212","225","246","250","268","286","288","290","308","315","328","330","335","340","348","355","360","365","375","400","410","412","430","456","456M","500","512","550","575","575M","599","612","612 Scaglietti","625","712","735","812","860","BB512","California","Daytona","Enzo","F333 SP","F355","F360","F40","F430","F50","F550","FF", "F12","GTB","GTO","GTS","Mondial","Spider","Superamerica","Testa Rossa");
+cars["Ford"] = new Array("Aerostar","Anglia","Aspire","Bronco","Bronco II","C-Max","Capri","Club Wagon","Cobra","Contour","Contour SVT","Cortina","Country Squire","Courier","Crown Victoria","Custom","Cutaway","E-100","E-150","E-250","E-350","Econoline Cargo","Econoline Wagon","Edge","Elite","Escape","Escape Hybrid","Escort","Esprit","Excursion","EXP","Expedition","Explorer","Explorer Sport","Explorer Sport T","F-100","F-150","F-250","F-250 Super Duty","F-350","F-350 Super Duty","F-450","F-550","F-650","F-750","Fairlane","Fairmont","Falcon","Festiva","Fiesta","Five Hundred","Flex","Focus","Focus SVT","Freestar","Freestyle","Fusion","Galaxie","Gran Torino","Granada","Grand Marquis","GT","GT40","Lightning","LTD","Marauder","Maverick","Model A","Model T","Mondeo","Mustang","Mustang SVT Cobr","Pinto","Probe","Puma","Ranchero","Ranger","Scorpio","Shelby","Sierra","Skyliner","Sport Track","Sunliner","T-Bucket","Taurus","Taurus Sho","Taurus X","Tempo","Territory","Thunderbird","Torino","Transit Connect","Tudor","Ute","Windstar","Windstar Cargo");
+cars["Honda"] = new Array("Accord","Civic","Civic CRX","Civic Del Sol","Civic Hybrid","Civic Si","Crosstour","CR-V","CRX","CR-Z","Element","Fit","FCX Clarity","FR-V","HR-V","Insight","Motorcycle","Odyssey","Passport","Pilot","Prelude","Ridgeline","S2000","S600","S800","Stream");
+cars["Hyundai"] = new Array("Accent","Azera","Elantra","Entourage","Excel","Genesis","Genesis Coupe","Santa Fe","Scoupe","Sonata","Terracan","Tiburon","Tucson","Veloster","Veracruz","XG300","XG350");
+cars["Jaguar"] = new Array("E-type","F-Type","MK","S-Type","S-Type R","Vanden Plas","X-Type","XF","XJ","XJ-S","XJ-SC","XJ12","XJ40","XJ6","XJ6 Serie 2","XJ6 Serie 3","XJ8","XJ8 Vanden Plas","XJR","XJS","XK","XK8","XKE","XKR","XR8");
+cars["Jeep"] = new Array("Cherokee","CJ","CJ-5","CJ-7","CJ5","CJ7","Comanche","Commander","Compass","Grand Cherokee","Grand Wagoneer","J-10 Pickup","J-20 Pickup","J10 Pickup","J20 Pickup","Liberty","Patriot","Renegade","Sahara","Scrambler","TJ","TJ Unlimited","Wagoneer","Wrangler","Wrangler Unlimited","YJ");
+cars["Kia"] = new Array("Amanti","Optima","Forte","Forte Koup","Picanto","Rio","Rondo","Sedona","Sephia","Sorento","Soul","Spectra","Sportage");
+cars["Lamborghini"] = new Array("350 GT","Cheetah","Countach","Diablo","Espada","Gallardo","Islero","Jalpa","Jarama","LM","Miura","Murcielago","Silhouette","Urraco");
+cars["Land Rover"] = new Array("Defender","Discovery","Discovery 3","Discovery II","Evoque","Freelander","LR2","LR3","LR4","Range Rover","Range Rover Sport");
+cars["Lexus"] = new Array("ES 250","ES 300","ES 330","ES 350","GS 300","GS 350","GS 400","GS 430","GS 450h","GX 470","IS 220","IS 250","IS 300","IS 350","LS 400","LS 430","LS 460","LS 600h L","LX 450","LX 470","RX 300","RX 330","RX 350","RX 400h","SC 300","SC 400","SC 430");
+cars["Maserati"] = new Array("3200 GT","3500 GT","420","430","5000 GT","Biturbo","Bora","Coupe","GrandCabrio","GranSport","GranSport Spyder","GrandTurismo","Khamsin","MC12","Merak","Quattroporte","Shamal","Spyder");
+cars["Mazda"] = new Array("2","3","323","5","6","6 Sport Wagon","616","626","929","B2000","B2200","B2300","B2500","B2600","B3000","B4000","Bravo 2600","Cosmo","CX-5","CX-7","CX-9","GLC","Mazda3","Mazda6","Millenia","MPV","MX-3","MX-5 Miata","MX-6","Navajo","Protege","Protege5","RX-3","RX-4","RX-7","RX-8","Tribute","Truck");
+cars["Mercedes-Benz"] = new Array("190-Class","240-Class","260-Class","280-Class","300-Class","350-Class","400-Class","420-Class","450-Class","500-Class","560-Class","600-Class","A-Class","B-Class","C-Class","C32 AMG","C36 AMG","C43 AMG","C55 AMG","CL-Class","CL55 AMG","CL65 AMG","CLK-Class","CLK55 AMG","CLS-Class","E-Class","E55 AMG","G-Class","GL-Class","GLK-Class","G55 AMG","M-Class","ML55 AMG","R-Class","S-Class","S55 AMG","SL-Class","SL55 AMG","SL65 AMG","SLK-Class","SLK32 AMG","SLK55 AMG","SLR McLaren","SLS-Class","Viano Van");
+cars["Mini"] = new Array("3000GT","ClassIC","Clubman","Cooper","Cooper Convertible","Countryman");
+cars["Mitsubishi"] = new Array("3000GT","Challenger","Colt","Conquest","Cordia","Diamante","Eclipse","Eclipse Spyder","Endeavor","Evolution","Expo","Express","Galant","Grandis","L200/Pick Up","L300","L400","Lancer","Lancer Sportback","Mighty Max","Mighty Max Picku","Mirage","Montero","Montero Sport","Outlander","Precis","Raider","Sigma","Starion","Tredia","Truck","Vanwagon");
+cars["Nissan"] = new Array("100NX","200SX","240SX","240Z","260Z","280ZX","300ZX","350Z","370Z","Altima","Armada","Axxess","Cube","Frontier","Gt-R","Juke","King Cab","Leaf","Maxima","Murano","Murano CrossCabriolet","NX","Pathfinder","Patrol","Pickup","Pulsar","Quest","Rogue","Sentra","Stanza","Titan","Van","Versa","X-Trail","Xterra");
+cars["Peugeot"] = new Array("1007","106","107","205","206","306","307","307 SW","309","405","406","407","500 Series","504","505","604","605","607","806","807","Boxer","Expert","J5","Partner","SW8","Wagon");
+cars["Porsche"] = new Array("356","550","901","911","911 Turbo","912","914","924","928","930","944","959","968","996","Boxster","Carrera","Carrera 911S","Carrera GT","Cayenne","Cayman","Panamera","Speedster","Spyder");
+cars["Renault"] = new Array("Alliance","Avantime","Clio","Dauphine","Encore","Espace","Fuego","Le Car","R10","R5","R8");
+cars["Subaru"] = new Array("B9","B9 Tribeca","Baja","Brat","DL","Forester","G3X Justy","GL","GL-10","GLF","Impreza","Impreza WRX STI","Justy","Legacy","Liberty","Loyale","Motorcycle","Outback","Outback Sport","RX","Standard","STi","SVX","WRX","WRX STi","XT","XT6");
+cars["Toyota"] = new Array("4Runner","Avalon","Camry","Celica","Corolla","Corona","Cressida","Echo","FJ Cruiser","Highlander","Land Cruiser","Matrix","MR2","MR2 Spyder","Paseo","Pickup","Previa","Prius","RAV4","Sequoia","Sienna","Solara","Starlet","Supra","T100","Tacoma","Tercel","Tundra","Van","Yaris");
+cars["Volkswagen"] = new Array("Beetle","Bus","CC","Cabrio","Cabriolet","Corrado","Dasher","Eos","Eurovan","Fox","Golf","Golf Cabriolet","GTI","Jetta","Jetta Wagon","Karman Ghia","New Beetle","New Beetle Cabri","Passat","Phaeton","Pickup","Quantum","R32","Rabbit","Routan","Scirocco","Thing","Touareg","Touran","Vanagon");
+cars["Volvo"] = new Array("160","240","242","244","245","260","340","360","440","460","480 ES","740","760","780","850","940","960","Bertone","C30","C70","Cross Country","DL","GL","GLE","GLT","GT","S40","S60","S60 R","S70","S80","S90","V40","V50","V70","V70 R","V90","XC","XC60","XC70","XC90");
+cars["Seat"] = new Array("Arona", "Leon", "Ibiza", "Alteca", "Leon Cupra R", "Leon Cupra", "Cordoba", "Toledo")
+
+jQuery(document).ready(function($){
+
+	$('span.text select').change(function(){
+		$(this).siblings('.value').text($(this).find('option[value="'+$(this).val()+'"]').text());
+	});
+
+	for ( make in cars )
+	{
+		$('#formmake').append('<option value="'+make+'">'+make+'</option>');
+	}
+
+	$('#formmake').change(function(){
+		var val = $(this).val();
+		$('#formmodel').html('<option value="">Select Model</option>');
+		for ( i in cars[val] )
+		{
+			$('#formmodel').append('<option value="'+cars[val][i]+'">'+cars[val][i]+'</option>');
+		}
+		$('#formmodel').append('<option value="Other">- Other -</option>');
+	});
+
+	$('#formmake, span.text select').each(function(){
+		var def = $(this).siblings('.value').text();
+		$(this).find('option[value='+def+']').attr('selected', 'selected');
+		$(this).change();
+	});
+});
